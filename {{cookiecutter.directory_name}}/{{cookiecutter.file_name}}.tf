87707aeae7281b0f2d4b6b6c6cc6485cd35c2c30
pencinarsanz-atos <paula.encinar.external@atos.net>
2021-09-10 12:35:07 +0000

    ADD correction

--- {{cookiecutter.directory_name}}/{{cookiecutter.file_name}}.tf
+++ {{cookiecutter.directory_name}}/{{cookiecutter.file_name}}.tf
@@ -1,7 +1,7 @@
 resource "kubernetes_pod" "dummy_netapp" {
   metadata {
     name = "{{cookiecutter.netapp_name}}"
-    namespace = "{{cookicutter.netapp_namespace}}"
+    namespace = "{{cookiecutter.netapp_namespace}}"
     labels = {
       app = "{{cookiecutter.netapp_app}}"
     }
@@ -9,23 +9,23 @@ resource "kubernetes_pod" "dummy_netapp" {
 
   spec {
     container {
-      image = "{{cookicutter.netapp_container_image}}"
-      name  = "{{cookicutter.netapp_container_name}}"
+      image = "{{cookiecutter.netapp_container_image}}"
+      name  = "{{cookiecutter.netapp_container_name}}"
     }
   }
 }
 
 resource "kubernetes_service" "dummy_netapp_service" {
   metadata {
-    name = "{{cookicutter.netapp_service}}"
+    name = "{{cookiecutter.netapp_service}}"
     namespace = "{{cookiecutter.netapp_namespace}}"
   }
   spec {
     selector = {
-      app = {{cookicutter.netapp_service_app}}
+      app = {{cookiecutter.netapp_service_app}}
     }
     port {
-      port = {{cookicutter.netapp_port}}
+      port = {{cookiecutter.netapp_port}}
       target_port = {{cookiecutter.netapp_target_port}}
     }
   }
