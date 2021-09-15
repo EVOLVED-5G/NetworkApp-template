ab84f030615590aeb3ed96c48d0cf35639c16f4b
pencinarsanz-atos <paula.encinar.external@atos.net>
2021-09-15 10:48:31 +0000

    ADD pre_gen hooks with git config --global

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -1,22 +1,8 @@
-# import re
-# import sys
-# import requests
-# import logging
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- 
+import os, sys
 
-# from requests.auth import HTTPBasicAuth
+os.system("git config --global user.email {{cookiecutter.git_email}}")
+os.system("git config --global user.name {{cookiecutter.remote_username}}")
 
-# MODULE_REGEX = r'^[_a-zA-Z][_a-zA-Z0-9]+$'
-
-# module_name = '{{ cookiecutter.module_name }}'
-
-# if not re.match(MODULE_REGEX, module_name):
-#     print('ERROR: %s is not a valid Python module name!' % module_name)
-
-#     # exits with status 1 to indicate failure
-#     sys.exit(1)
-
-# my_token = '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'
-# payload = {"name": "{{cookiecutter.repo_slug}}","private":"true"}
-# head = {'Authorization': 'token {}'.format(my_token)}
-# r = requests.post('https://api.github.com/user/repos',headers=head, json=payload)
-# print(r)
+print("He añadido los git config")
